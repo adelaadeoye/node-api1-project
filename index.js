3022303b92286524b53c125afcb86db8321f123b
adeoye adela <adeoye.adela@gmail.com>
2019-12-02 21:39:16 +0100

    PORT 4000 created

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 // implement your API here
 console.log("Server Test");
+
+const express= require("express");
+const db =require('./data/db.js');
+
+
+
+const server= express();
+
+server.use(express.json());
+
+
+
+
+
+
+
+
+
+// Create port for listening 
+const port = 4000;
+server.listen(port, () => console.log(`\n ** API on port${port}**\n`));

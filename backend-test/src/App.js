6852fd1b9860d92fa2498ab9b23b1d1f4ed095ab
adeoye adela <adeoye.adela@gmail.com>
2019-12-03 00:15:36 +0100

    delete button working

--- backend-test/src/App.js
+++ backend-test/src/App.js
@@ -6,6 +6,7 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import axios from "axios";
 import { Typography } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -31,7 +32,21 @@ function App() {
       })
       .catch(error => console.log(error));
   }, []);
+const handleDelete=(id)=>{
 
+axios
+      .delete(`http://localhost:4000/api/users/${id}`)
+      .then(res => {
+        axios
+      .get("http://localhost:4000/api/users")
+      .then(res => {
+        console.log(res.data);
+        setUser(res.data);
+      })
+      .catch(error => console.log(error));
+      })
+      .catch(error => console.log(error));
+}
   return (
     <div className="App">
       <header className="App-header">
@@ -41,18 +56,21 @@ function App() {
         </p>
         <div className={classes.root}>
           <Grid container spacing={3}>
-            {user.map(item => 
-               <Grid key={item.id} item xs={6}>
-               <Paper className={classes.paper}>
-                 <Typography variant="h5" component="h2">
-                  Name: {item.name}
-                 </Typography>
-                 <Typography variant="h6" component="h2">
-                  Bio: {item.bio}
-                 </Typography>
-               </Paper>
-             </Grid>
-            )}
+            {user.map(item => (
+              <Grid key={item.id} item xs={6}>
+                <Paper className={classes.paper}>
+                  <Typography variant="h5" component="h2">
+                    Name: {item.name}
+                  </Typography>
+                  <Typography variant="h6" component="h2">
+                    Bio: {item.bio}
+                  </Typography>
+                  <Button variant="contained" color="secondary" onClick={()=>handleDelete(item.id)}>
+                    Delete
+                  </Button>
+                </Paper>
+              </Grid>
+            ))}
           </Grid>
         </div>
       </header>
